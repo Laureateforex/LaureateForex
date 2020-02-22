ad2dad9fb339f6959475a6ea4386d607408c53a4
Matt <laureateinvestments@gmail.com>
2020-02-22 08:02:58 +0000

    RSI calc help

--- MD.RSI_Calc.py
+++ MD.RSI_Calc.py
@@ -1,41 +1,27 @@
-rsi_Indictor(close,n_days):
-    rsi_series = pd.DataFrame(close)
+import statistics
+n = 14
+
+change = (EURGBP_Data["Price"] - EURGBP_Data["Price"].shift(1))
+
+Up = change > 0
+Down = abs(change < 0)
+
+average_up = statistics.mean([1:n = 1]) #NEED to create panda of ups and downs for this to work??? what you think Reza bro?
+# above I am sorting if up of if down movement (need abs value but need to know if down or up) then need list these??
+
+average_down = statistics.mean([1:n = 1])
+
+RS = average_up / average_down
 
 
-    # Change = close[i]-Change[i-1]
-    rsi_series["Change"] = (rsi_series["Close"] - rsi_series["Close"].shift(1)).fillna(0)
+"""
+Up = [15, 9, 55, 41, 35, 20, 62, 49]
+Down = [3, 6, 2, 9, 1, 15, 20, 50]
 
-    # Upword Movement
-    rsi_series["Upword Movement"] = (rsi_series["Change"][rsi_series["Change"] >0])
-    rsi_series["Upword Movement"] = rsi_series["Upword Movement"].fillna(0)
+x = statistics.mean(Up)
+y = statistics.mean(Down)
 
-    # Downword Movement
-    rsi_series["Downword Movement"] = (abs(rsi_series["Change"])[rsi_series["Change"] <0]).fillna(0)
-    rsi_series["Downword Movement"] = rsi_series["Downword Movement"].fillna(0)
+RS = x / y
 
-    #Average Upword Movement
-    # For first Upword Movement Mean of first n elements.
-    rsi_series["Average Upword Movement"] = 0.00
-    rsi_series["Average Upword Movement"][n] = rsi_series["Upword Movement"][1:n+1].mean()
-
-    # For Second onwords
-    for i in range(n+1,len(rsi_series),1):
-        #print(rsi_series["Average Upword Movement"][i-1],rsi_series["Upword Movement"][i])
-        rsi_series["Average Upword Movement"][i] = (rsi_series["Average Upword Movement"][i-1]*(n-1)+rsi_series["Upword Movement"][i])/n
-
-    #Average Downword Movement
-    # For first Downword Movement Mean of first n elements.
-    rsi_series["Average Downword Movement"] = 0.00
-    rsi_series["Average Downword Movement"][n] = rsi_series["Downword Movement"][1:n+1].mean()
-
-    # For Second onwords
-    for i in range(n+1,len(rsi_series),1):
-        #print(rsi_series["Average Downword Movement"][i-1],rsi_series["Downword Movement"][i])
-        rsi_series["Average Downword Movement"][i] = (rsi_series["Average Downword Movement"][i-1]*(n-1)+rsi_series["Downword Movement"][i])/n
-
-    #Relative Index
-    rsi_series["Relative Strength"] = (rsi_series["Average Upword Movement"]/rsi_series["Average Downword Movement"]).fillna(0)
-
-    #RSI
-    rsi_series["RSI"] = 100 - 100/(rsi_series["Relative Strength"]+1)
-    return rsi_series.round(2)
+RSI = 100 - (100 / (RS + 1))
+print(RSI) """
