8ed50dd05dcf975d6f40a3bb24228f44506f88fa
Matt <laureateinvestments@gmail.com>
2020-02-24 20:36:36 +0000

    Updated

--- Read.data.py
+++ Read.data.py
@@ -1,11 +1,11 @@
 import pandas as pd
+LFX_series = pd.Series()
 
 data = pd.read_csv(r'EUR GBP Historical Data')
 
 Close = data["Price"].astype(float)
 
-LFX_series = pd.Series()
-
 LFX_series["Change"] = (Close - Close.shift(1)).fillna(0)
 
 print(LFX_series)
+
