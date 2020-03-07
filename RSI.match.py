ff22443de0670f7a070f3e5cb0e5967b1a719d79
Matt <laureateinvestments@gmail.com>
2020-03-07 15:20:39 +0000

    upgrade

--- RSI.match.py
+++ RSI.match.py
@@ -5,8 +5,8 @@ LFX_hourly = pd.DataFrame()
 LFX_match = pd.DataFrame()
 n = 14
 
-data_daily = pd.read_csv(r'exchange.data.csv')
-data_hourly = pd.read_csv(r'EURUSD data from web.csv')
+data_daily = pd.read_csv(r'Daily_data.csv')
+data_hourly = pd.read_csv(r'hourly_data.csv')
 
 
 def rsi_daily(price):
@@ -17,27 +17,30 @@ def rsi_daily(price):
     LFX_daily["Up"] = (LFX_daily["Change"][LFX_daily["Change"] > 0])
     LFX_daily["Up"] = LFX_daily["Up"].fillna(0)
 
-
     LFX_daily["Down"] = (abs(LFX_daily["Change"])[LFX_daily["Change"] < 0]).fillna(0)
     LFX_daily["Down"] = LFX_daily["Down"].fillna(0)
-
 
     LFX_daily["Ave Up"] = 0.00
     LFX_daily["Ave Up"][n] = LFX_daily["Up"][1:n+1].mean()
 
-
-    for i in range(n+1,len(LFX_daily),1):
+    for i in range(n+1, len(LFX_daily), 1):
         LFX_daily["Ave Up"][i] = (LFX_daily["Ave Up"][i-1]*(n-1)+LFX_daily["Up"][i])/n
 
     LFX_daily["Ave Down"] = 0.00
     LFX_daily["Ave Down"][n] = LFX_daily["Down"][1:n+1].mean()
 
-    for i in range(n+1,len(LFX_daily),1):
+    for i in range(n+1, len(LFX_daily), 1):
         LFX_daily["Ave Down"][i] = (LFX_daily["Ave Down"][i-1]*(n-1)+LFX_daily["Down"][i])/n
 
     LFX_daily["RS"] = (LFX_daily["Ave Up"]/LFX_daily["Ave Down"]).fillna(0)
 
     LFX_daily["RSI"] = 100 - 100/(LFX_daily["RS"]+1)
+
+    LFX_daily.loc[LFX_daily["RSI"] < 33, "match"] = "sell"
+    LFX_daily.loc[LFX_daily["RSI"] < 15, "match"] = "wait"
+    LFX_daily.loc[LFX_daily["RSI"] > 66, "match"] = "buy"
+    LFX_daily.loc[LFX_daily["RSI"] > 80, "match"] = "wait"
+    LFX_daily["match"] = LFX_daily["match"].fillna("wait")
     return LFX_daily
 
 
@@ -49,14 +52,11 @@ def rsi_hourly(price):
     LFX_hourly["Up"] = (LFX_hourly["Change"][LFX_hourly["Change"] > 0])
     LFX_hourly["Up"] = LFX_hourly["Up"].fillna(0)
 
-
     LFX_hourly["Down"] = (abs(LFX_hourly["Change"])[LFX_hourly["Change"] < 0]).fillna(0)
     LFX_hourly["Down"] = LFX_hourly["Down"].fillna(0)
 
-
     LFX_hourly["Ave Up"] = 0.00
     LFX_hourly["Ave Up"][n] = LFX_hourly["Up"][1:n+1].mean()
-
 
     for i in range(n+1,len(LFX_daily),1):
         LFX_hourly["Ave Up"][i] = (LFX_hourly["Ave Up"][i-1]*(n-1)+LFX_hourly["Up"][i])/n
@@ -70,26 +70,26 @@ def rsi_hourly(price):
     LFX_hourly["RS"] = (LFX_hourly["Ave Up"]/LFX_hourly["Ave Down"]).fillna(0)
 
     LFX_hourly["RSI"] = 100 - 100/(LFX_hourly["RS"]+1)
+
+    LFX_hourly.loc[LFX_hourly["RSI"] < 33, "match"] = "sell"
+    LFX_hourly.loc[LFX_hourly["RSI"] < 15, "match"] = "wait"
+    LFX_hourly.loc[LFX_hourly["RSI"] > 66, "match"] = "buy"
+    LFX_hourly.loc[LFX_hourly["RSI"] > 80, "match"] = "wait"
+    LFX_hourly["match"] = LFX_hourly["match"].fillna("wait")
+    return LFX_hourly
+
+
+def open_position():
+    if LFX_daily["match"][0] == "buy" and LFX_hourly["match"][0] == "buy":
+        print("go long")
+    elif LFX_daily["match"][0] == "sell" and LFX_hourly["match"][0] == "sell":
+        print("go short")
+    else:
+        print("wait")
     return LFX_hourly
 
 
 rsi_daily("EUR/USD Close")
 rsi_hourly("EUR/USD Close")
-"""print(LFX_daily)
-print(LFX_hourly)
-
-for i in LFX_daily["RSI"]:
-    if i > 66:
-        LFX_daily["match"] = "buy"
-    elif i < 33:
-        LFX_daily["match"] = "short"
-    else:
-        LFX_daily["match"] = "wait son - the cycle repeats" """
-
-
-for i in LFX_daily["RSI"]:
-    if i > 66:
-        LFX_daily["buy"] = "yes"
-        break
-
-print(LFX_daily)
+open_position()
+print(open_position)
