23b8b607f6e027b342e5ca798623b6e1c4273034
Matt <laureateinvestments@gmail.com>
2020-04-06 11:25:59 +0100

    Order sampe Response[400]

--- Oanda_ORDERsample.py
+++ Oanda_ORDERsample.py
@@ -19,25 +19,39 @@ import logging
 token = "3943fda13fb4e7085832a01eadfee5ef-3a1a62e075a3e0301b996ae8a632e63b"
 accountID = "101-004-13417875-001"
 
-
+import requests
 
 if __name__ == "__main__":
     api = API(access_token=token, environment="practice")
 
+    domain = 'api-fxpractice.oanda.com'
+    access_token = "3943fda13fb4e7085832a01eadfee5ef-3a1a62e075a3e0301b996ae8a632e63b"
+    account_id = "101-004-13417875-001"
+    Pair = "EUR_USD"
+
+    url = "https://" + domain + "/v3/accounts/" + account_id + "/orders"
+    header = {"Authorization": "Bearer " + access_token}
+
+
     ordr = MarketOrderRequest(instrument="EUR_USD",
-                              units=1)
+                              units=100)
 
     order_buy = json.dumps(ordr.data, indent=4)
 
-    ro = orders.OrderCreate(accountID=accountID, data=order_buy)
+    """ro = orders.OrderCreate(accountID=accountID, data=order_buy)
     print("REQUEST:{}".format(ro))
     print("====================")
     print("r.data")
 
-    try:
+    response = api.request(ro)"""
+
+    requestdata = requests.post(url, headers=header, params=order_buy)
+    print(requestdata)
+
+    """try:
         response = api.request(ro)
     except V20Error as e:
         print("V20Error: {}".format(e))
     else:
         print("Response: {}\n{}".format(ro.status_code,
-                                        json.dumps(response, indent=2)))
+                                        json.dumps(response, indent=2)))"""
